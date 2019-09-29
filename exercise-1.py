0d465b51062481c877fc645a51c6a504e2de2bf0
unknown <saipranav36@gmail.com>
2019-09-29 12:24:56 -0500

    part-2

--- exercise-1.py
+++ exercise-1.py
@@ -5,3 +5,12 @@ aircrafts = {'x' : 21, 'y' : 41 }
 print(aircrafts['x'])
 
 print(aircrafts['y'])
+
+#Exercise-1 part-2
+#fleet = {'a' = [aircraft1, aircraft2, aircraft3, aircraft4, aircraft5]}
+
+fleet = {'x' :[63,65,67,69,71], 'y' :[63,65,67,69,71] }
+
+print(fleet['x'])
+
+print(fleet['y'])
