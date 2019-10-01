109e663114d55a20a0b9468251af6321904fb8bd
unknown <saipranav36@gmail.com>
2019-10-01 10:03:54 -0500

    created four methods

--- exercise-2.py
+++ exercise-2.py
@@ -1,8 +1,20 @@
-# create an class Aircraft
+## create an class Aircraft
 class Aircraft:
-	#initializing variables
-	def __init__(self,x=0,y=0,ac=1):
-		self.x = x
-		self.y = y
-		self.ac = ac
-
+    #initializing variables
+    def __init__(self,x=0,y=0,ac=1):
+        self.x=x
+        self.y=y
+        self.ac=ac
+    # creating methods
+    def move_left(self):
+        print('moved left . . .')
+        self.x = self.x - self.ac
+    def move_right(self):
+        print('moved right . . .')
+        self.x = self.x + self.ac
+    def move_up(self):
+        print('moved up . . .')
+        self.y = self.y + self.ac
+    def move_down(self):
+        print('moved down')
+        self.y = self.y - self.ac
